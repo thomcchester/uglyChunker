df258195829d99cd7ce235c528ca6276c8ad0153
Thomas Chester <thomas.charles.chester@gmail.com>
2017-09-11 21:51:08 -0500

    add delete

--- client/scripts/controllers/inputcontrol.js
+++ client/scripts/controllers/inputcontrol.js
@@ -1,12 +1,19 @@
 App.controller('InputController',  ['$scope', '$log', '$http', '$window', '$mdSidenav', "$mdDialog", "$mdMedia", 'ClientService',function($scope, $log, $http, $window, $mdSidenav, $mdDialog, $mdMedia, ClientService) {
   var service = ClientService;
 
-  $scope.taskNumber = [1,2,3];
-
-  $scope.task = {};
-
-  // $scope.$watchCollection()
-  $scope.crown = function(){console.log($scope.task.name)};
-
+  $scope.allTasks=[];
+  $scope.taskAdd=function(){
+    $scope.allTasks.push({taskName:$scope.taskName,taskType:$scope.taskType,taskValue:$scope.taskValue})
+    $scope.taskName = "";
+    $scope.taskType = "";
+    $scope.taskValue = "";
+  }
+  $scope.remove=function(x){
+    for(var i=$scope.allTasks.length-1;i>=0;i--){
+      if($scope.allTasks[i].taskName===x){
+        $scope.allTasks.splice(i,1)
+      }
+    }
+  }
 
 }]);
